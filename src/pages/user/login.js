9db3366e33012bc7ce9a175e8a43445122d07e2b
taeyomi <100909703+taeyomi@users.noreply.github.com>
2024-07-15 21:45:10 +0900

    헤더 수정/레이아웃 수정

--- src/pages/user/login.js
+++ src/pages/user/login.js
@@ -1,11 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
 import naverImg from '../../assets/img/naver.png';
 import loginLine from '../../assets/img/loginLine.png';
 import kakaoImg from '../../assets/img/kakao.png';
 import googleImg from '../../assets/img/google.png'
+import axios from "axios";
+
 
 function Login() {
+
+  let api = 'http://localhost:8080';
+  // 카카오
+  const KakaoLogin = () => {
+    axios.get(`${api}/api/v1/login/kakao`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('에러가 발생했습니다!', error);
+      });
+  };
+
+  // 구글 로그인 요청
+  const GoogleLogin = () => {
+    axios.get(`${api}/api/v1/login/google`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('에러가 발생했습니다!', error);
+      });
+  };
+
+  
+  
+  const loginHandlerKakao = () => {
+    KakaoLogin();
+  };
+  
+  const loginHandlerGoogle = () => {
+    GoogleLogin();
+  };
+  
+
+
+  //구글
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
       <div className="text-2xl font-bold mb-4 mt-[50px] ">
@@ -21,13 +60,11 @@ function Login() {
       </div>
       
       <div className="flex flex-col items-center mt-[20px]  ">
-        <button className="p-4">
-          <img src={naverImg} alt="Naver"  />
-        </button>
-        <button className="p-4">
+       
+        <button className="p-4" onClick={loginHandlerKakao} >
           <img src={kakaoImg} alt="Kakao"/>
         </button>
-        <button className="p-4">
+        <button className="p-4" onClick={loginHandlerGoogle}>
           <img src={googleImg} alt="Google"  />
         </button>
       </div>
