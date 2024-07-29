5b20219eab19aa1c4f00d6d00ca3aaaa30b74ab0
taeyomi <100909703+taeyomi@users.noreply.github.com>
2024-07-29 16:24:00 +0900

    채팅 시간

--- src/pages/chat/chatroom.js
+++ src/pages/chat/chatroom.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
+import back from '../../assets/img/back_.png';
 import { Client } from '@stomp/stompjs';
 import { Stomp } from "@stomp/stompjs";
 import Side from '../../components/side';
 import SockJS from 'sockjs-client';
+import SendImg from '../../assets/img/chatSend.png';
 
 
 const Chatroom = () => {
   // URL에서 채팅방 ID를 가져옴
   const { id } = useParams();
   const roomId = id;
-
+  const navigate = useNavigate();
   
 
   // 채팅 메시지 상태
@@ -121,37 +124,56 @@ console.log(memberId);
   
   console.log(memberId);
   console.log(message);
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      sendMessage();
+    }
+  };
 
+  const handleBackClick = () => {
+    navigate(-1); // 이전 페이지로 이동
+  };
 
   
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       <Side />
       <div className="fixed top-0 left-[670px] w-[512px] h-[calc(100vh-3px)] bg-[#F9F8F8] shadow-2xl rounded-[30px] flex flex-col overflow-hidden no-scrollbar z-10">
-        <div className="bg-[#6fbf73] p-4 text-white font-bold text-center">
-          채팅방 {roomId}
+        <div className="bg-[#6fbf73] p-4 text-white font-bold text-center flex items-center justify-between">
+          <img src={back} alt="back" className="w-6 h-6 cursor-pointer" onClick={handleBackClick}/>
+          <span className="flex-grow text-center">채팅방 {roomId}</span>
+          <div className="w-6 h-6"></div>
         </div>
         <div className="flex-1 overflow-y-auto p-4" style={{ marginBottom: '100px' }}>
-          {Array.isArray(messages) && messages.map((msg, index) => (
-            <div key={index} className={`mb-4 p-2 ${msg.owner === 'me' ? 'bg-blue-200 text-right ml-auto' : 'bg-gray-200 text-left mr-auto'} rounded-lg w-fit max-w-[60%]`}>
-              {msg.message}
-            </div>
-          ))}
+        {Array.isArray(messages) && messages.map((msg, index) => {
+            const time = new Date(msg.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            return (
+              <div>
+              <div key={index} className={`mb-4 p-4 ${msg.owner === 'me' ? 'bg-white text-right ml-auto' : 'bg-[#E7F2EC] text-left mr-auto'} rounded-[30px] w-fit max-w-[60%]`}>
+                <div>{msg.message}</div>  <div><div className="text-xs text-gray-500 mt-1">{time}</div></div>
+               
+              </div>
+            
+              </div>
+              
+            );
+          })}
           <div ref={messagesEndRef} />
         </div>
         <div className="fixed bottom-0 left-[670px] w-[512px] bg-white border-t p-4 flex">
           <input
             type="text"
-            className="flex-1 p-2 border rounded-lg"
+  className="flex-1 p-2 border rounded-[30px] bg-[#E7F2EC] focus:border-[#E7F2EC] outline-none"
             placeholder="메시지를 입력하세요"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyPress}
           />
           <button
-            className="ml-2 p-2 bg-[#6fbf73] text-white rounded-lg"
+            className='w-12 h-12'
             onClick={sendMessage}
           >
-            보내기
+              <img src={SendImg} alt="SendImg" />
           </button>
         </div>
       </div>
